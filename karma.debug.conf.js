9f596f0895797afe43492da17a1eb94ff5f5a19b
Mattia <mattia.belletti@gmail.com>
2015-08-11 16:59:55 +0200

    Use non-minified files in tests debug

--- karma.debug.conf.js
+++ karma.debug.conf.js
@@ -2,7 +2,7 @@ module.exports = function(config) {
 	config.set({
 		frameworks: ['jasmine'],
 		files: [
-			'node_modules/angular/angular.min.js',
+			'node_modules/angular/angular.js',
 			'node_modules/angular-mocks/angular-mocks.js',
 			'src/*.js',
 			'tests/*-helper.js',
